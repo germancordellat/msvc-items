c03faf94a0e3d105c2bc6b2fab65856dec07ddc6
germancordellat <germancordellat@gmail.com>
2025-12-30 17:20:08 +0100

    Implementar métodos findAll y findById en ItemServiceWebClient

--- src/main/java/com/prueba/msvc/items/services/ItemServiceWebClient.java
+++ src/main/java/com/prueba/msvc/items/services/ItemServiceWebClient.java
@@ -1,12 +1,17 @@
 package com.prueba.msvc.items.services;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.prueba.msvc.items.models.Item;
 
+@Service
 public class ItemServiceWebClient implements ItemService {
 
     private final WebClient.Builder client;
@@ -17,14 +22,25 @@ public class ItemServiceWebClient implements ItemService {
 
     @Override
     public List<Item> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        return client.build().get()
+                .uri("http://msvc-products")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux(Item.class)
+                .collectList()
+                .block();
     }
 
     @Override
     public Optional<Item> findById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        Map<String, Long> params = new HashMap<>();
+        params.put("id", id);
+        return client.build().get()
+                .uri("http://msvc-products/{id}", params)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(Item.class)
+                .blockOptional();
     }
 
 }
