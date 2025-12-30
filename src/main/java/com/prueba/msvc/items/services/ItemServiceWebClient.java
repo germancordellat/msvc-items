4c5b62c6e0644a6a7e072b445600d8bffd68fbd4
germancordellat <germancordellat@gmail.com>
2025-12-30 17:11:17 +0100

    Agregar configuración de WebClient y servicio ItemServiceWebClient

---
+++ src/main/java/com/prueba/msvc/items/services/ItemServiceWebClient.java
@@ -0,0 +1,30 @@
+package com.prueba.msvc.items.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.prueba.msvc.items.models.Item;
+
+public class ItemServiceWebClient implements ItemService {
+
+    private final WebClient.Builder client;
+
+    public ItemServiceWebClient(WebClient.Builder client) {
+        this.client = client;
+    }
+
+    @Override
+    public List<Item> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+}
