4f40c91d92b44c1fa9e76bde4c7c6e1ae2bd512c
germancordellat <germancordellat@gmail.com>
2025-12-30 17:28:26 +0100

    Corregir tipos de retorno en ItemServiceWebClient para usar Product en lugar de Item

--- src/main/java/com/prueba/msvc/items/services/ItemServiceWebClient.java
+++ src/main/java/com/prueba/msvc/items/services/ItemServiceWebClient.java
@@ -5,12 +5,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.prueba.msvc.items.models.Item;
+import com.prueba.msvc.items.models.Product;
 
+@Primary
 @Service
 public class ItemServiceWebClient implements ItemService {
 
@@ -26,7 +29,10 @@ public class ItemServiceWebClient implements ItemService {
                 .uri("http://msvc-products")
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
-                .bodyToFlux(Item.class)
+                .bodyToFlux(Product.class)
+                .map(product -> 
+                    new Item(product, 1)
+                )
                 .collectList()
                 .block();
     }
@@ -39,7 +45,8 @@ public class ItemServiceWebClient implements ItemService {
                 .uri("http://msvc-products/{id}", params)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
-                .bodyToMono(Item.class)
+                .bodyToMono(Product.class)
+                .map(product -> new Item(product, 1))
                 .blockOptional();
     }
 
