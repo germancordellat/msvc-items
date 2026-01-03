4b6ba8a2803e05a814a66ef5d73cc0eab218bb2c
germancordellat <germancordellat@gmail.com>
2026-01-03 11:56:25 +0100

    Agregar configuración de Spring Cloud y nuevos métodos en ItemController para manejar configuraciones dinámicas

--- src/main/java/com/prueba/msvc/items/controllers/ItemController.java
+++ src/main/java/com/prueba/msvc/items/controllers/ItemController.java
@@ -9,10 +9,16 @@ import com.prueba.msvc.items.services.ItemService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,12 +26,18 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-
+@RefreshScope
 @RestController
 public class ItemController {
 
     private final ItemService itemService;
     private final CircuitBreakerFactory circuitBreakerFactory;
+
+    @Value("${configuracion.texto}")
+    private String text;
+
+    @Autowired
+    private Environment env;
 
     public ItemController(ItemService itemService, CircuitBreakerFactory circuitBreakerFactory) {
         this.itemService = itemService;
@@ -38,6 +50,19 @@ public class ItemController {
         System.out.println("token-request: " + tokenRequest);
         return itemService.findAll();
     }
+
+    @GetMapping("/fetch-configs")
+    public ResponseEntity<?> fetchConfigs(@Value("${server.port}") String port) {
+        Map<String, String> json = new HashMap<>();
+        json.put("text", text);
+        json.put("port", port);
+        if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+            json.put("author.name", env.getProperty("configuracion.autor.nombre"));
+            json.put("author.email", env.getProperty("configuracion.autor.email"));
+        }
+        return ResponseEntity.ok(json);
+    }
+    
 
     @GetMapping("/{id}")
     public ResponseEntity<Item> details(@PathVariable Long id) {
